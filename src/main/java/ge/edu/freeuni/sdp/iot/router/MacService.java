59d8bf9cfb3c98ec5627c0d712b9e41ce065533d
nikoloz14 <nakhv12@yahoo.com>
2016-06-24 23:35:49 +0400

    post and list added, needs test

--- src/main/java/ge/edu/freeuni/sdp/iot/router/MacService.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/router/MacService.java
@@ -10,9 +10,18 @@ import java.util.UUID;
 @Path("/houses/{house_id}")
 public class MacService {
 
+    private House getHouseById(Houses houses, int houseId) {
+        House house = houses.getHouse(houseId);
+        if (house == null) {
+            house = new House(houseId);
+            houses.addHouse(house);
+        }
+        return house;
+    }
+
     @GET
     @Path("/available")
-    public Response get(@PathParam("house_id") String houseId) {
+    public Response get(@PathParam("house_id") int houseId) {
         return Response.ok().entity("{\n" +
                 "    \"atHome\": true\n" +
                 "  }").build();
@@ -20,32 +29,26 @@ public class MacService {
 
     @POST
     @Path("/connect")
-    public Response create(@PathParam("house_id") String houseId, MacObject macObject) {
+    public Response create(@PathParam("house_id") int houseId, MacObject macObject) {
+        Houses houses = Houses.getInstance();
+        House house = getHouseById(houses, houseId);
         final String uniqueId = UUID.randomUUID().toString();
+        macObject.setId(uniqueId);
+        house.addMac(macObject);
         return Response.ok().entity(macObject.toString()).build();
     }
 
     @DELETE
     @Path("/connect/{mac_id}")
-    public Response create(@PathParam("house_id") String houseId, @PathParam("mac_id") String macId) {
+    public Response create(@PathParam("house_id") int houseId, @PathParam("mac_id") String macId) {
         return Response.ok().build();
     }
 
     @GET
     @Path("/addresses")
-    public Response getAddresses(@PathParam("house_id") String houseId) {
-        return Response.ok().entity("[\n" +
-                "            {\n" +
-                "                \"deviceName\": \"USER-PC\",\n" +
-                "                \"deviceMacAddress\": \"00:0a:95:9d:68:16\",\n" +
-                "                \"mac_id\": qengimdsmf,\n" +
-                "            },\n" +
-                "            \n" +
-                "            {\n" +
-                "                \"deviceName\": \"USER2-PC\",\n" +
-                "                \"deviceMacAddress\": \"01:0b:22:fd:18:1w\",\n" +
-                "                \"mac_id\": asniasdanmsd\n" +
-                "            }\n" +
-                "        ]").build();
+    public Response getAddresses(@PathParam("house_id") int houseId) {
+        Houses houses = Houses.getInstance();
+        House house = getHouseById(houses, houseId);
+        return Response.ok().entity(houses.toString()).build();
     }
 }
