be4b3dac15fcb01fbd6757a71676d96aa9383d85
nikoloz14 <nakhv12@yahoo.com>
2016-06-27 00:01:44 +0400

    added CORS headers for browser

--- src/main/java/ge/edu/freeuni/sdp/iot/router/MacService.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/router/MacService.java
@@ -2,13 +2,22 @@ package ge.edu.freeuni.sdp.iot.router;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 /**
  * Created by Nikoloz on 06/24/16.
  */
+
+
 @Path("/houses/{house_id}")
 public class MacService {
+
+    private static final String LOCAL = "http://localhost:8080";
+    private static final String HEROKU = "http://iot-webui.herokuapp.com";
+    private Set<String> originSet = new HashSet<>(Arrays.asList(LOCAL, HEROKU));
 
     private House getHouseById(Houses houses, int houseId) {
         House house = houses.getHouse(houseId);
@@ -21,40 +30,49 @@ public class MacService {
 
     @GET
     @Path("/available")
-    public Response get(@PathParam("house_id") int houseId) {
+    public Response get(@PathParam("house_id") int houseId, @HeaderParam("Origin") String origin) {
         Houses houses = Houses.getInstance();
         House house = getHouseById(houses, houseId);
-        return Response.ok().entity("{\"atHome\":" + house.isAnynoneAtHome() + "}").build();
+        return addHeaders(Response.ok().entity("{\"atHome\":" + house.isAnynoneAtHome() + "}"), origin);
     }
 
     @POST
     @Path("/connect")
-    public Response create(@PathParam("house_id") int houseId, MacObject macObject) {
+    public Response create(@PathParam("house_id") int houseId, MacObject macObject, @HeaderParam("Origin") String origin) {
         Houses houses = Houses.getInstance();
         House house = getHouseById(houses, houseId);
         final String uniqueId = UUID.randomUUID().toString();
         macObject.setId(uniqueId);
         house.addMac(macObject);
-        return Response.ok().entity(macObject.toString()).build();
+        return addHeaders(Response.ok().entity(macObject.toString()), origin);
     }
 
     @DELETE
     @Path("/connect/{mac_id}")
-    public Response delete(@PathParam("house_id") int houseId, @PathParam("mac_id") String macId) {
+    public Response delete(@PathParam("house_id") int houseId, @PathParam("mac_id") String macId, @HeaderParam("Origin") String origin) {
         Houses houses = Houses.getInstance();
         House house = getHouseById(houses, houseId);
         boolean succ = house.removeMac(macId);
         if (succ)
-            return Response.ok().build();
+            return addHeaders(Response.ok(), origin);
         else
-            return Response.status(404).build();
+            return addHeaders(Response.status(404), origin);
     }
 
     @GET
     @Path("/addresses")
-    public Response getAddresses(@PathParam("house_id") int houseId) {
+    public Response getAddresses(@PathParam("house_id") int houseId, @HeaderParam("Origin") String origin) {
         Houses houses = Houses.getInstance();
         House house = getHouseById(houses, houseId);
-        return Response.ok().entity(house.toString()).build();
+        return addHeaders(Response.ok().entity(house.toString()), origin);
+    }
+
+    private Response addHeaders(Response.ResponseBuilder rpBuilder, String origin){
+        if (originSet.contains(origin)) {
+            rpBuilder.header("Access-Control-Allow-Origin", origin);
+            rpBuilder.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+            rpBuilder.header("Access-Control-Allow-Headers", "Content-Type");
+        }
+        return rpBuilder.build();
     }
 }
